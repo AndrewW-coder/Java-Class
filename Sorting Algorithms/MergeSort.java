3ec362f3cee0930229ac7e8a842a62530b45978f
AndrewW-coder <Its.Andrew.Wang.2007@gmail.com>
2022-07-09 15:39:26 -0700

    Added notes for static and non-static methods, started mergesort

---
+++ Sorting Algorithms/MergeSort.java
@@ -0,0 +1,15 @@
+public class MergeSort {
+    public static void main(String[] args) {
+
+    }
+
+    public static void mergeSort() {
+
+    }
+
+    public static void swap(int[] arr, int x, int y) {
+        int temp = arr[y];
+        arr[y] = arr[x];
+        arr[x] = temp;
+    }
+}
